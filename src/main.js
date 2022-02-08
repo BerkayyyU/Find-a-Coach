9fb9478e888e1235c513d1f5b53edf387ab1dfb7
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-02-08 22:18:45 +0300

    initial routes & pages created

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { createApp } from 'vue';
+import routes from './router';
+import App from './App.vue';
 
-createApp({}).mount('#app');
+createApp(App).use(routes).mount('#app');
---
+++ src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Coach Details</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
