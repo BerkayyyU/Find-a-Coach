474f214732541b3d6af86eb07e56f8bda07a6dda
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-02-12 19:28:32 +0300

    register as a coach to firebase and get coaches from db reqs

--- src/store/modules/coaches.js
+++ src/store/modules/coaches.js
@@ -3,24 +3,24 @@ const coachesModule = {
   state() {
     return {
       coaches: [
-        {
-          id: 'c1',
-          firstName: 'Maximilian',
-          lastName: 'Schwarzmüller',
-          areas: ['frontend', 'backend', 'career'],
-          description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-          hourlyRate: 30,
-        },
-        {
-          id: 'c2',
-          firstName: 'Julie',
-          lastName: 'Jones',
-          areas: ['frontend', 'career'],
-          description:
-            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
-          hourlyRate: 30,
-        },
+        // {
+        //   id: 'c1',
+        //   firstName: 'Maximilian',
+        //   lastName: 'Schwarzmüller',
+        //   areas: ['frontend', 'backend', 'career'],
+        //   description:
+        //     "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+        //   hourlyRate: 30,
+        // },
+        // {
+        //   id: 'c2',
+        //   firstName: 'Julie',
+        //   lastName: 'Jones',
+        //   areas: ['frontend', 'career'],
+        //   description:
+        //     'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+        //   hourlyRate: 30,
+        // },
       ],
     };
   },
@@ -28,18 +28,64 @@ const coachesModule = {
     registerCoach(state, payload) {
       state.coaches.push(payload);
     },
+
+    setCoaches(state, payload) {
+      state.coaches = [...payload];
+    },
   },
   actions: {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+      const userId = context.rootGetters.userId;
       const coachData = {
-        id: context.rootGetters.userId,
         firstName: data.first,
         lastName: data.last,
         description: data.desc,
         hourlyRate: data.rate,
         areas: data.areas,
       };
-      context.commit('registerCoach', coachData);
+
+      const response = await fetch(
+        `https://vue-http-demo-47447-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json `,
+        {
+          method: 'PUT',
+          body: JSON.stringify(coachData),
+        }
+      );
+      // const responseData = await response.json();
+
+      if (!response.ok) {
+        //error
+      }
+
+      context.commit('registerCoach', { ...coachData, id: userId });
+    },
+    async loadCoaches(context) {
+      const res = await fetch(
+        'https://vue-http-demo-47447-default-rtdb.europe-west1.firebasedatabase.app/coaches.json',
+        { method: 'GET' }
+      );
+
+      const resData = await res.json();
+
+      if (!res.ok) {
+        //error
+      }
+
+      const coaches = [];
+
+      for (const key in resData) {
+        const coach = {
+          id: key,
+          firstName: resData[key].firstName,
+          lastName: resData[key].lastName,
+          description: resData[key].description,
+          hourlyRate: resData[key].hourlyRate,
+          areas: resData[key].areas,
+        };
+        coaches.push(coach);
+      }
+
+      context.commit('setCoaches', coaches);
     },
   },
   getters: {
