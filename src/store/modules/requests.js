8b7fc069d105d00ff8e681cc7eef4eeb00b3857f
Berkay Ulguel <berkayyyulguel@gmail.com>
2022-02-23 16:20:37 +0300

    send and fetch coach requests from firebase

--- src/store/modules/requests.js
+++ src/store/modules/requests.js
@@ -9,16 +9,56 @@ export const requestsModule = {
     addRequest(state, payload) {
       state.requests.push(payload);
     },
+    setRequests(state, payload) {
+      state.requests = payload;
+    },
   },
   actions: {
-    contactCoach(context, payload) {
+    async contactCoach(context, payload) {
       const newRequest = {
-        id: new Date().toISOString(),
-        coachId: payload.coachId,
         userEmail: payload.email,
         message: payload.message,
       };
+      const res = await fetch(
+        `https://vue-http-demo-47447-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.coachId}.json`,
+        { method: 'POST', body: JSON.stringify(newRequest) }
+      );
+      const resData = await res.json();
+
+      if (!res.ok) {
+        const error = new Error(resData.message || 'Failed to send request.');
+        throw error;
+      }
+
+      newRequest.id = resData.name;
+      newRequest.coachId = payload.coachId;
       context.commit('addRequest', newRequest);
+    },
+
+    async fetchRequests(context) {
+      const coachId = context.rootGetters.userId;
+      const res = await fetch(
+        `https://vue-http-demo-47447-default-rtdb.europe-west1.firebasedatabase.app/requests/${coachId}.json`
+      );
+      const resData = await res.json();
+
+      if (!res.ok) {
+        const error = new Error(resData.message || 'Failed to fetch request.');
+        throw error;
+      }
+      const requests = [];
+
+      for (const key in resData) {
+        const request = {
+          id: key,
+          coachId: coachId,
+          userEmail: resData[key].userEmail,
+          message: resData[key].message,
+        };
+        requests.push(request);
+      }
+
+      context.commit('setRequests', requests);
     },
   },
   getters: {
